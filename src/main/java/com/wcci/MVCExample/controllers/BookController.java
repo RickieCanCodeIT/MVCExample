dc597d5907292dcf94147150d35249a640ec3919
davidHooper-WCCI <david@wecancodeit.org>
2022-02-17 09:37:53 -0700

    end of thursday, added form for reviews

--- src/main/java/com/wcci/MVCExample/controllers/BookController.java
+++ src/main/java/com/wcci/MVCExample/controllers/BookController.java
@@ -4,8 +4,7 @@ import com.wcci.MVCExample.entities.Book;
 import com.wcci.MVCExample.repos.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -20,11 +19,11 @@ public class BookController {
     @RequestMapping("/")
     public String showBooksTemplate(Model model) {
         model.addAttribute("books", bookRepo.findAll());
+        model.addAttribute("student","Avery");
         return "BooksTemplate";
     }
 
-
-    @RequestMapping("/books/{id}")
+    @GetMapping("/books/{id}")
     public String showBookTemplate(Model model, @PathVariable long id) {
         Optional<Book> tempBook = bookRepo.findById(id);
         if (tempBook.isPresent())
@@ -32,6 +31,13 @@ public class BookController {
             model.addAttribute("inBook", tempBook.get());
         }
         return "BookTemplate";
+    }
+    @PostMapping("/books/{id}")
+    public String addReview(@PathVariable long id, @RequestParam String review){
+        Book mybook = bookRepo.findById(id).get();
+        mybook.addReview(review);
+        bookRepo.save(mybook);
+        return "redirect:/books/"+ id;
     }
 
     @RequestMapping("/books/title/{title}")
